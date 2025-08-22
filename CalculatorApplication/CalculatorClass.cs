8c811d010b33dccb84fa8e3b4f7e629de64af863
awitgaming <156307787+awitgaming@users.noreply.github.com>
2025-08-22 23:28:43 +0800

    Step 4: Create class and declare generic delegate

---
+++ CalculatorApplication/CalculatorClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApplication
+{
+    public delegate T Formula<T>(T num1,  T num2);
+    internal class CalculatorClass
+    {
+
+    }
+}
