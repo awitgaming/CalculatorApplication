da8273f0829a2bf662deae6b5bf4d58f43fca72d
awitgaming <156307787+awitgaming@users.noreply.github.com>
2025-08-22 23:39:39 +0800

    Step 6 & 7: create 2 method, add event successor named CalculateEvent

--- CalculatorApplication/CalculatorClass.cs
+++ CalculatorApplication/CalculatorClass.cs
@@ -10,5 +10,30 @@ namespace CalculatorApplication
     internal class CalculatorClass
     {
         public Formula<double> formula;
+
+        private Formula<double> calculateEvent;
+        public event Formula<double> CalculateEvent
+        {
+            add 
+            {
+                Console.WriteLine("Added The Delegate");
+                CalculateEvent += value;
+            }
+
+            remove
+            {
+                Console.WriteLine("Remove The Delegate");
+                CalculateEvent -= value;
+            }
+        }
+
+
+        public double GetSum(double num1, double num2) { 
+            return num1 + num2;
+        }
+
+        public double GetDifference(double num1, double num2) { 
+            return num1 - num2;
+        }
     }
 }
